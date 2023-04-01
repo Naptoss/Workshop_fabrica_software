953446bfed51f512b8faa1aacd418986e2984150
Naptoss <tomgabinio@gmail.com>
2023-04-01 09:15:05 -0300

    alterado o nome da funcao e o metodo

--- contatos/views.py
+++ contatos/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def contatos(request):
-    return HttpResponse("Hello world")
+def index(request):
+    return render(request, 'contatos/index.html')
