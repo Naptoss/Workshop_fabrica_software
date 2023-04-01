2f3e32682562043f5cc3c801178b5bc0a45dde1b
Naptoss <tomgabinio@gmail.com>
2023-04-01 00:43:56 -0300

    Beggin

--- contatos/apps.py
+++ contatos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
+class ContatosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'contatos'
